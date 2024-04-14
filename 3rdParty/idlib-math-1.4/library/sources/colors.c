0e9fe877040d7d9f9281ac2dc5f64b810e338591
Michael Heilmann <michael.heilmann@shizu.org>
2024-04-14 16:51:52 +0200

    update third party library
    Update third party library "idlib-math" to version 1.4.

--- 3rdParty/idlib-math-1.4/library/sources/colors.c
+++ 3rdParty/idlib-math-1.4/library/sources/colors.c
@@ -2,9 +2,9 @@
 #include "colors.h"
 
 #define DEFINE(NAME, R, G, B) \
-  static_assert(0 <= R && R <= 255, "<internal error>"); \
-  static_assert(0 <= G && G <= 255, "<internal error>"); \
-  static_assert(0 <= B && B <= 255, "<internal error>"); \
+  _Static_assert(0 <= R && R <= 255, "<internal error>"); \
+  _Static_assert(0 <= G && G <= 255, "<internal error>"); \
+  _Static_assert(0 <= B && B <= 255, "<internal error>"); \
   idlib_color_3_u8 const idlib_colors_##NAME##_3_u8 = { .r = R, .g = G, .b = B };
 
 #include "colors.i"
