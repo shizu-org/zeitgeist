0940ef82b7a64417854a892648e83a4198bccca6
Michael Heilmann <michael.heilmann@shizu.org>
2024-04-14 16:51:43 +0200

    add upstream requests

---
+++ Library/Zeitgeist/UpstreamRequests.c
@@ -0,0 +1,38 @@
+// Copyright (c) 2024 Michael Heilmann. All rights reserved.
+
+#include "Zeitgeist/UpstreamRequests.h"
+
+#include "Zeitgeist.h"
+// malloc, free
+#include <malloc.h>
+
+Zeitgeist_UpstreamRequest*
+Zeitgeist_UpstreamRequest_createExitProcessRequest
+  (
+    Zeitgeist_State* state
+  )
+{
+  Zeitgeist_UpstreamRequest* upstreamRequest = malloc(sizeof(Zeitgeist_UpstreamRequest));
+  if (!upstreamRequest) {
+    state->lastError = 1;
+    longjmp(state->jumpTarget->environment, -1);
+  }
+  upstreamRequest->type = Zeitgeist_UpstreamRequestType_ExitProcess;
+  ((Zeitgeist_Object*)upstreamRequest)->finalize = NULL;
+  ((Zeitgeist_Object*)upstreamRequest)->next = state->objects;
+  state->objects = (Zeitgeist_Object*)upstreamRequest;
+  return upstreamRequest;
+}
+
+
+void
+Zeitgeist_sendUpstreamRequest
+  (
+    Zeitgeist_State* state,
+    Zeitgeist_UpstreamRequest* request
+  )
+{ 
+  if (Zeitgeist_UpstreamRequestType_ExitProcess == request->type) {
+    state->exitProcess = true;
+  }
+}
---
+++ Library/Zeitgeist/UpstreamRequests.h
@@ -0,0 +1,52 @@
+// Copyright (c) 2024 Michael Heilmann. All rights reserved.
+
+#if !defined(ZEITGEIST_UPSTREAMREQUESTS_H_INCLUDED)
+#define ZEITGEIST_UPSTREAMREQUESTS_H_INCLUDED
+
+#include "Zeitgeist/Object.h"
+#include "Zeitgeist/Value.h"
+
+/**
+ * @since 0.1
+ * @brief An upstream request is send from an Rendition to Zeitgeist.
+ * The only upstream
+ */
+typedef struct Zeitgeist_UpstreamRequest Zeitgeist_UpstreamRequest;
+
+/**
+ * @since 0.1
+ * Symbolic constant that when assigned to Zeitgeist_UpstreamRequest indicates that it is a "exit process" upstream reuqest.
+ */
+#define Zeitgeist_UpstreamRequestType_ExitProcess (1)
+
+struct Zeitgeist_UpstreamRequest {
+  Zeitgeist_Object _parent;
+  Zeitgeist_Integer type;
+};
+
+/**
+ * @since 0.1
+ * @brief Create a "exit process" upstream request.
+ * @param state A pointer to the Zeitgeist_State object.
+ * @return A pointer to the Zeitgeist_UpstreamRequest object.
+ */
+Zeitgeist_UpstreamRequest*
+Zeitgeist_UpstreamRequest_createExitProcessRequest
+  (
+    Zeitgeist_State* state
+  );
+
+/**
+ * @since 0.1
+ * @brief Send an upstream request.
+ * @param state A pointer to the Zeitgeist_State object.
+ * @param state A pointer to the Zeitgeist_UpstreamRequest object.
+ */
+void
+Zeitgeist_sendUpstreamRequest
+  (
+    Zeitgeist_State* state,
+    Zeitgeist_UpstreamRequest* request
+  );
+
+#endif // ZEITGEIST_UPSTREAMREQUESTS_H_INCLUDED
